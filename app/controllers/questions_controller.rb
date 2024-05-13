1599b31d11ff107c9e2d3d4fc7089fd90a4a3002
bryan chuttoo <chuttoobryan04@gmail.com>
2024-05-13 12:38:00 +0400

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @questions = params[:questions]
+    if @questions == "I am going to work"
+      @answer = "Great"
+    elsif @questions.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!."
+    else
+      @answer = "I don't care!"
+    end
+  end
+end
